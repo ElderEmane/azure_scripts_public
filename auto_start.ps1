ff7563301e84d38496af1bc4a5666635828906dc
emane <elderemane@gmail.com>
2025-02-11 11:26:52 +0100

    fixes to autostop and start scripts

--- auto_start.ps1
+++ auto_start.ps1
@@ -3,10 +3,10 @@ Import-Module Az.Resources
 Import-Module Az.Compute
 
 function Connect {
-        $null = Connect-AzAccount -WarningAction SilentlyContinue -Verbose:$false
-        $subscriptions = Get-AzSubscription | Select-Object Id, Name
+    $null = Connect-AzAccount -WarningAction SilentlyContinue -Verbose:$false
+    $subscriptions = Get-AzSubscription | Select-Object Id, Name
 
-        return $subscriptions
+    return $subscriptions
 }
 
 function tag_adhoc {
@@ -23,11 +23,12 @@ function tag_24/5 {
         [string]$VMname,
         [string]$VMrg
     )
-    $convertedDateTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($currentDateTime, $timezone[$key])
-    $hour = $convertedDateTime.ToString("HH") + "00"
+    $hour = (Get-Date).ToString("HHmm")
+    $dayOfWeek = (Get-Date).DayOfWeek
 
-    if ($hour -eq "0000" -and (Get-Date).DayOfWeek -eq [System.DayOfWeek]::Monday){
+    if ($dayOfWeek -ge [System.DayOfWeek]::Monday -and $dayOfWeek -le [System.DayOfWeek]::Friday -and ($hour -ge "0000" -and $hour -le "2359")) {
         Start-AzVM -ResourceGroupName $VMrg -Name $VMname
+        Write-Host "starting vm" $VMname
     }
 }
 
@@ -39,6 +40,7 @@ function tag_adhoc_24/5 {
     Write-Output $VMrg $VMname
     if ((Get-Date).DayOfWeek -ge [System.DayOfWeek]::Monday -and (Get-Date).DayOfWeek -le [System.DayOfWeek]::Friday) {
         Start-AzVM -ResourceGroupName $VMrg -Name $VMname
+        Write-Host "starting vm" $VMname
     }
     else {
         Write-Output "invalid day of the week"
@@ -49,11 +51,16 @@ function tag_business_hours{
         [string]$VMname,
         [string]$VMrg
     )
+
     $convertedDateTime = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId($currentDateTime, $timezone[$key])
     $hour = $convertedDateTime.ToString("HH") + "00"
 
-    if ($hour -eq "0000" -and (Get-Date).DayOfWeek -ge [System.DayOfWeek]::Monday -and (Get-Date).DayOfWeek -le [System.DayOfWeek]::Friday){
+
+    $dayOfWeek = (Get-Date).DayOfWeek
+
+    if ($dayOfWeek -ge [System.DayOfWeek]::Monday -and $dayOfWeek -le [System.DayOfWeek]::Friday -and $hour -ge "0600") {
         Start-AzVM -ResourceGroupName $VMrg -Name $VMname
+        Write-Host "starting vm" $VMname
     }
 
 }
@@ -100,8 +107,10 @@ function autostart {
                             Invoke-Expression $command
                         }
                         else {
-                            if ($operation_hours -match "24/5" -or $operation_hours -match "business_hours" ){
+                            if ($operation_hours -eq "24/5" -or $operation_hours -eq "business_hours" ){
+                                Write-Host "24/5 or businnes_hours"
                                 $command = "tag_$operation_hours -VMname `"$VMname`" -VMrg `"$VMrg`""
+                                Invoke-Expression $command
                             }
                             else {
                                 Write-Host "no tag"
@@ -111,7 +120,7 @@ function autostart {
                 }
             }
             else {
-                Write-Output ($VMname + " is 24/7")
+                Write-Output "24/7"
             }
         }
     }
